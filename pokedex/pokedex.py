793715906db58e6a8deba98ec95948d5afd44e53
Loretta <lcheung44@my.bcit.ca>
2020-01-15 23:19:12 -0800

    Updated color output

--- pokedex/pokedex.py
+++ pokedex/pokedex.py
@@ -26,36 +26,39 @@ class PokeGUI:
         self.pokeimg_frame.place(relx=0.285, rely=0.28, relwidth=0.19, relheight=0.11, anchor='n')
 
         # label for pokemon image frame
-        self.pokeimg_label = None
+        self.pokeimg_label = tk.Label(self.pokeimg_frame, anchor='w', bg="#013df5")
+        # must keep a reference per https://effbot.org/tkinterbook/photoimage.htm
+        self.pokeimg_label.image = ""
+        self.pokeimg_label.place(relwidth=1, relheight=1)
 
         # container for info frame
         self.info_frame = tk.Frame(self.root)
         self.info_frame.place(relx=0.55, rely=0.30, relwidth=0.32, relheight=0.1, anchor='n')
 
         # label for info frame
-        self.info_label = tk.Label(self.info_frame, bg="red", font=('Courier New', 14), anchor=tk.NW, justify='left')
+        self.info_label = tk.Label(self.info_frame, bg="#013df5", font=('Courier New', 14, 'bold'), anchor=tk.NW, justify='left')
         self.info_label.place(relwidth=1, relheight=1)
 
         # container for abilities frame
         self.abilities_frame = tk.Frame(self.root)
-        self.abilities_frame.place(relx=0.45, rely=0.39, relwidth=0.525, relheight=0.10, anchor='n')
+        self.abilities_frame.place(relx=0.46, rely=0.39, relwidth=0.515, relheight=0.10, anchor='n')
 
         # label for abilities frame
-        self.abilities_label = tk.Label(self.abilities_frame, bg="yellow", font=('Courier New', 14), anchor=tk.NW, justify='left', wraplength=220)
+        self.abilities_label = tk.Label(self.abilities_frame, bg="#013df5", font=('Courier New', 14, 'bold'), anchor=tk.NW, justify='left', wraplength=220)
         self.abilities_label.place(relwidth=1, relheight=1)
 
         # container for text entry field
-        self.mid_frame = tk.Frame(self.root)
+        self.mid_frame = tk.Frame(self.root, bg="#5ab16a")
         self.mid_frame.place(relx=0.37, rely=0.685, relwidth=0.29, relheight=0.095, anchor='n')
 
-        self.entry = tk.Entry(self.mid_frame, font=('Courier New', 15))
+        self.entry = tk.Entry(self.mid_frame, font=('Courier New', 15), bg="#5ab16a", highlightthickness=0, justify="center")
         self.entry.place(relwidth=1, relheight=1)
 
         # container for search button
         self.lower_frame = tk.Frame(self.root)
         self.lower_frame.place(relx=0.37, rely=0.80, relwidth=0.3, relheight=0.04, anchor='n')
 
-        self.button = tk.Button(self.lower_frame, text="Search", font=('Courier New', 15), command=lambda: APIHandler.get_data(self, self.entry.get()))
+        self.button = tk.Button(self.lower_frame, text="Search", font=('Courier New', 15, "bold"), command=lambda: APIHandler.get_data(self, self.entry.get()))
         self.button.place(relheight=1, relwidth=1)
 
         # # container for error msg frame
@@ -104,21 +107,14 @@ class APIHandler:
                 gui.info_label['text'] = info
 
                 img_url = pokemon['sprites']['front_default']
-                print(img_url)
-
                 img_response = requests.get(img_url)
                 img_data = img_response.content
-                # poke_img = ImageTk.PhotoImage(Image.open(BytesIO(img_data)))
-
                 poke_img = Image.open(BytesIO(img_data))
                 resized_img = poke_img.resize((80, 80), Image.ANTIALIAS)
                 poke_img = ImageTk.PhotoImage(resized_img)
 
-                gui.pokeimg_label = tk.Label(gui.pokeimg_frame, bg="blue", anchor='w', image=poke_img)
-
-                # must keep a reference per https://effbot.org/tkinterbook/photoimage.htm
+                gui.pokeimg_label['image'] = poke_img
                 gui.pokeimg_label.image = poke_img
-                gui.pokeimg_label.place(relwidth=1, relheight=1)
 
                 abilities = "Abilities:"
                 for index in range(len(pokemon['abilities'])):
